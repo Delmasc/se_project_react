b63defd04bbe1782a9a7b6f5f105da5a3f607173
delmas cox <delmaslcox1@gamil.com>
2025-11-26 11:02:42 -0500

    check

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,17 @@
+import WeatherCard from "../WeatherCard/WeatherCard";
+
+function Main() {
+  return (
+    <Main>
+      <WeatherCard />
+      <section className="cards">
+        <p className="cards__text">
+          Today is 75 &deg; F / You may want to wear:
+        </p>
+        {/*  Todo Add the cards */}
+      </section>
+    </Main>
+  );
+}
+
+export default Main;
