c7df509c039969b7c81ba0f66532609f8c794c39
delmas cox <delmaslcox1@gamil.com>
2025-12-09 10:33:14 -0500

    add project

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,16 +1,28 @@
-import WeatherCard from "../WeatherCard/WeatherCard";
+import "./Main.css";
 
-function Main() {
+import WeatherCard from "../WeatherCard/WeatherCard";
+import ItemCard from "../ItemCard/ItemCard";
+import { defaultClothingItems } from "../../utils/constants";
+
+function Main({ weatherData, handleCardClick }) {
   return (
-    <Main>
-      <WeatherCard />
+    <main>
+      <WeatherCard weatherData={weatherData} />
       <section className="cards">
         <p className="cards__text">
-          Today is 75 &deg; F / You may want to wear:
+          Today is {weatherData.temp.F} &deg; F / You may want to wear:
         </p>
-        {/*  Todo Add the cards */}
+        <ul className="cards__list">
+          {defaultClothingItems
+            .filter((item) => {
+              return item.weather === weatherData.type;
+            })
+            .map((item) => {
+              return <ItemCard key={item._id} item={item} onCardClick={handleCardClick} />;
+            })}
+        </ul>
       </section>
-    </Main>
+    </main>
   );
 }
 
--- src/components/WeatherCard/WeatherCard.jsx
+++ src/components/WeatherCard/WeatherCard.jsx
@@ -1,5 +1,38 @@
-function WeatherCard() {
-  return <section className="weather"></section>;
+import "./WeatherCard.css";
+import { weatherOptions, defaultWeatherOptions } from "../../utils/constants";
+
+function WeatherCard({ weatherData }) {
+  const filteredOptions = weatherOptions.filter((option) => {
+    return (
+      option.day === weatherData.isDay &&
+      option.condition === weatherData.condition
+    );
+  });
+
+  let weatherOption;
+  if (filteredOptions.length === 0) {
+    weatherOption = defaultWeatherOptions[weatherData.isDay ? "day" : "night"];
+  } else {
+    weatherOption = filteredOptions[0];
+  }
+
+  console.log(weatherData);
+
+  // const weatherOptionUrl = filteredOptions[0]?.url;
+  // const weatherOptionCondition = filteredOptions[0]?.condition;
+  // const weatherOptionDay = filteredOptions[0]?.day;
+
+  return (
+    <section className="weather-card">
+      <p className="weather-card__temp">{weatherData.temp.F} &deg; F</p>
+      <img
+        src={weatherOption?.url}
+        alt={`Card showing ${weatherOption?.day ? "day" : "night"} time ${weatherOption?.condition
+          } weather`}
+        className="weather-card__image"
+      />
+    </section>
+  );
 }
 
 export default WeatherCard;
